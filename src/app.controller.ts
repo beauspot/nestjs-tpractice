ec2b0ac719e395774b85d6ca07a245d6c8bf7658
beauspot <beauspot@outlook.com>
2024-01-02 11:48:21 +0100

    slightly modified the app.controller module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,30 +15,60 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 
+// Importing the services
+import { AppService } from './app.service';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { IncomeReport, ExpenseReport } from './interface/reports';
+import { data } from './db/data';
+import { ReportType } from './interface/data';
+import { v4 as uuid } from 'uuid';
+
+const dbFilePath = path.resolve(process.cwd(), 'data.json');
+
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.getAllReportServices(reportType);
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.getReportByIDService(reportType, id);
   }
 
   @Post()
-  createReport() {
-    return 'Created';
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReportService(reportType, { amount, source });
   }
 
   @Put(':id')
-  editReportById() {
-    return 'edited';
+  updateReportById(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.updateReportService(reportType, id, body);
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReportByID() {
-    return 'Report Deleted';
+  deleteReportByID(@Param('id') id: string) {
+    return this.appService.deleteReportService(id);
   }
 }
