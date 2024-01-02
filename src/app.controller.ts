da682c444a814738ade4402fc680d0734b0fa175
beauspot <beauspot@outlook.com>
2024-01-03 00:10:52 +0100

    took out the code and put them in their seperate directories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,71 +1,10 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Delete,
-  Body,
-  Param,
-  ParseEnumPipe,
-  HttpCode,
-  HttpStatus,
-  ParseUUIDPipe,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 // Importing the services
 import { AppService } from './app.service';
-import { ReportType } from './interface/data';
-import { CreateReportDTO, UpdateReportDTO } from './DTO/report.dto';
 
-@Controller('report/:type')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getAllReports(@Param('type', new ParseEnumPipe(ReportType)) type: string) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return this.appService.getAllReportServices(reportType);
-  }
-
-  @Get(':id')
-  getReportById(
-    @Param('type', new ParseEnumPipe(ReportType)) type: string,
-    @Param('id', ParseUUIDPipe) id: string,
-  ) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return this.appService.getReportByIDService(reportType, id);
-  }
-
-  @HttpCode(201)
-  @Post()
-  createReport(
-    @Body() { amount, source }: CreateReportDTO,
-    @Param('type', new ParseEnumPipe(ReportType)) type: string,
-  ) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return this.appService.createReportService(reportType, { amount, source });
-  }
-
-  @Put(':id')
-  updateReportById(
-    @Param('type', new ParseEnumPipe(ReportType)) type: string,
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() body: UpdateReportDTO,
-  ) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return this.appService.updateReportService(reportType, id, body);
-  }
-
-  @HttpCode(204)
-  @Delete(':id')
-  deleteReportByID(@Param('id', ParseUUIDPipe) id: string) {
-    return this.appService.deleteReportService(id);
-  }
-}
+@Controller()
+export class AppController {}
