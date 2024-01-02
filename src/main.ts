4080efe206e3178683043c65bd65733fc3794618
beauspot <beauspot@outlook.com>
2024-01-02 13:52:28 +0100

    Added the validationPipe method for validating data.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './config/app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 const Port = AppConfig.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(Port);
 }
 bootstrap();
