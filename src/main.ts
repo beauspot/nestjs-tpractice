fe0b28e968096af0597e9679a480d3bffafeec38
beauspot <beauspot@outlook.com>
2024-01-02 11:48:57 +0100

    the main entery point of the application

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './config/app.config';
-import { ValidationPipe } from '@nestjs/common';
 
 const Port = AppConfig.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(Port);
 }
 bootstrap();
