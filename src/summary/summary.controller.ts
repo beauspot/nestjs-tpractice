eff4328c3a9056ba885bf902c9f176b214253c9b
beauspot <beauspot@outlook.com>
2024-01-06 17:33:05 +0100

    implemented the summary controller on the application.

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+import { Controller, Get } from '@nestjs/common';
+import { SummaryService } from './summary.service';
 
 @Controller('summary')
-export class SummaryController {}
+export class SummaryController {
+  // Returns a totoal of our income
+  // a total of our expenses
+  // and the net-income = otal income - total expense
+  constructor(private readonly summaryService: SummaryService) {}
+  @Get()
+  getSumamry() {
+    return this.summaryService.calculateSummary();
+  }
+}
