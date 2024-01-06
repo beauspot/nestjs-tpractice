df91a7b614ea9af793cee9eb68e22ed65a6c9712
beauspot <beauspot@outlook.com>
2024-01-06 17:39:06 +0100

    finished setting up the calculateSummary services and exporting it to its respective controller.

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
 import { Injectable } from '@nestjs/common';
+import { ReportService } from '../report/report.service';
+import { ReportType } from 'src/interface/data';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  calculateSummary() {
+    const totalExpense = this.reportService
+      .getAllReportServices(ReportType.EXPENSE)
+      .reduce((sum, report) => sum + report.amount, 0);
+    const totalIncome = this.reportService
+      .getAllReportServices(ReportType.INCOME)
+      .reduce((sum, report) => sum + report.amount, 0);
+    return {
+      totalIncome,
+      totalExpense,
+      netIncome: totalIncome - totalExpense,
+    };
+  }
+}
